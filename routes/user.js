36441f07a331d510d450e60727b1a532ca7ceb0c
Nibir Nilav Bora <nibirbora03@gmail.com>
2024-10-14 22:28:48 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,13 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../json.js') 
-const { signIn, signUp } = require('../controlers/user.js')
+const { signIn, signUp, addUser } = require('../controlers/user.js')
 const router = express.Router()
 
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
-
+router.put('/add/:id',addUser);
 
 
 module.exports = router;
